5d8de63e52ab34a69566c550ecf528e50d9d270d
clara <yuhui.xu@epitech.eu>
2022-03-21 19:09:33 +0100

    events mouse positions

--- ninja_course/app.js
+++ ninja_course/app.js
@@ -4,12 +4,24 @@ new Vue({
         name: 'Clara',
         job: 'ninja',
         age: 18,
+        x: 0,
+        y: 0,
         website: 'https://www.google.fr',
         websiteTag: '<a href="https://www.google.fr">Ninja One</a>'
     },
     methods: {
         greet: function(str) {
             return str + this.name
+        },
+        add: function(val) {
+            this.age += val
+        },
+        sub: function(val) {
+            this.age -= val
+        },
+        updateXY: function(event) {
+            this.x = event.offsetX
+            this.y = event.offsetY
         }
     }
 });
